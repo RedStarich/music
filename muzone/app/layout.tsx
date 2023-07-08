321ca43daf4f4bc2980187205a95d27002892c5f
RedStarich <altairzhambyl@gmail.com>
2023-07-08 09:50:44 +0600

    set up environment

---
+++ muzone/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'MuZone',
+  description: 'My Sound',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
