3179d413ca94eb1edcc309a737025f1b7e38b388
RedStarich <altairzhambyl@gmail.com>
2023-07-08 17:12:17 +0600

    Log in
    Log in and log out functions completed, authorization via GitHub is added

--- muzone/components/Header.tsx
+++ muzone/components/Header.tsx
@@ -8,6 +8,10 @@ import {HiHome} from "react-icons/hi";
 import {BiSearch} from "react-icons/bi";
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import {toast} from "react-hot-toast";
 
 
 
@@ -23,9 +27,19 @@ const Header: React.FC<HeaderProps> = ({
     const authModal = useAuthModal();
     const router = useRouter();
 
-    const handleLogout = () =>{
-        //handle logout in the future
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
 
+    const handleLogout = async () =>{
+        const { error } = await supabaseClient.auth.signOut();
+        // TODO: reset any playing songs
+        router.refresh();
+
+        if (error) {
+            toast.error(error.message);
+        } else {
+            toast.success('Logged Out!')
+        }
     }
     return (
         <div
@@ -125,33 +139,50 @@ const Header: React.FC<HeaderProps> = ({
                         gap-x-4
                     "
                 >
-                    <>
-                        <div>
+                    {user ? (
+                        <div className="flex gap-x-4 items-center">
+                            <Button
+                                onClick={handleLogout}
+                                className="bg-white px-6 py-2"
+                            >
+                                Logout
+                            </Button>
+                            <Button
+                                onClick={() => router.push('/account')}
+                                className="bg-white"
+                            >
+                                <FaUserAlt />
+                            </Button>
+                        </div>
+                    ): (
+                        <>
+                            <div>
+                                <Button
+                                    onClick={authModal.onOpen}
+                                    className="
+                                        bg-transparent
+                                        text-neutral-300
+                                        font-medium
+                                    "
+                                >
+                                    Sign Up
+                                </Button>
+                            </div>
+                            <div>
                             <Button
                                 onClick={authModal.onOpen}
                                 className="
-                                    bg-transparent
-                                    text-neutral-300
+                                    bg-white
+                                    px-6
+                                    py-2
                                     font-medium
                                 "
                             >
-                                Sign Up
+                                Log In
                             </Button>
-                        </div>
-                        <div>
-                        <Button
-                            onClick={authModal.onOpen}
-                            className="
-                                bg-white
-                                px-6
-                                py-2
-                                font-medium
-                            "
-                        >
-                            Log In
-                        </Button>
-                        </div>
-                    </>
+                            </div>
+                        </>
+                    )}
 
                 </div>
             </div>
